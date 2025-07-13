5f90ebb53f2457d472c343e2b4af2cfabbbc243a
James Morrissey <jammim19@gmail.com>
2025-07-13 13:25:56 +0100

    Made the utils file PEP8 compliant

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
+"""Utils File."""
+
 import os
 import logging
 
+
 def create_folder_from_title(title: str, thread_id: str) -> str:
-    """Create a sanitized folder name from the page title and thread id and make's the folder."""
+    """Create  folder name from the page title and thread id."""
     # "/3/ - Pixel - 3DCG - chan"
 
     # TODO This could be removed later, and placed into 'split_up_page_title()'
@@ -14,25 +17,23 @@ def create_folder_from_title(title: str, thread_id: str) -> str:
     #     parts = parts[1:-2]  # Remove first and last two parts
     #     folder_name = "".join(parts)
     # TODO Down to here
-        
-
 
     # folder_name = folder_name.replace("/", "_").strip()
 
     folder_name_with_id = f"{title}-{thread_id}"
 
-    if not os.path.exists(os.path.join(os.getenv("OUTPUT_FOLDER"),folder_name_with_id)):
-        os.mkdir(os.path.join(os.getenv("OUTPUT_FOLDER"),folder_name_with_id))
+    folder_path = os.path.join(os.getenv("OUTPUT_FOLDER"), folder_name_with_id)
+
+    if not os.path.exists(folder_path):
+        os.mkdir(os.path.join(os.getenv("OUTPUT_FOLDER"), folder_name_with_id))
         logging.info(f"Created folder: {folder_name_with_id}")
     else:
         logging.info(f"Folder already exists: {folder_name_with_id}")
     return folder_name_with_id
 
 
-
-
-def split_up_page_title(title:str) -> list[str]:
-
+def split_up_page_title(title: str) -> list[str]:
+    """Return an array of broken up title elements."""
     trimmed_title = title.replace("/", "").strip()
 
     split_page_title = trimmed_title.split(" - ")
